91a8860e3568549e565aa51b5168e4b89c84ebff
rodrigospbr <87215430+rodrigospbr@users.noreply.github.com>
2024-08-16 16:03:16 -0300

    Create Tables.sql

---
+++ Tables.sql
@@ -0,0 +1,15 @@
+---script de criação de tabelas
+---
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [BI].[Tb_erros](
+	[ErroDescricao] [nvarchar](500) NULL,
+	[ErroCodigo] [int] NULL,
+	[ErroColuna] [int] NULL
+	[ErroTexto] [nvarchar(MAX)]
+) ON [PRIMARY]
+GO
